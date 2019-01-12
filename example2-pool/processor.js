8138451ad087232361713d61630c9a538aa27c24
Dmitry Papka <dmitrypapka@Dmitrys-MacBook-Pro.local>
2019-01-12 13:35:37 -0300

    Printing thread id

--- example2-pool/processor.js
+++ example2-pool/processor.js
@@ -30,7 +30,7 @@ class Processor {
 
 function startWorker() {
     parentPort.on('message', message => setTimeout(() => {
-        console.log(`Number processed ${message}`);
+        console.log(`Number processed: ${message}`);
         process.exit();
     }, 1000));
 }
