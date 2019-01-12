43af82f8bbfc7ae8eaf51ab6b6eb6f744d49b602
Dmitry Papka <dmitrypapka@Dmitrys-MacBook-Pro.local>
2019-01-12 16:38:14 -0300

    Example 6 - listerning for console.log and console.error from worker

--- example6-error-and-output-streams/app.js
+++ example6-error-and-output-streams/app.js
@@ -4,5 +4,3 @@ const worker = new Worker('./worker.js', { stdout: true, stderr: true });
 
 worker.stdout.on('data', data => console.log(`Out stream received data: ${data}`));
 worker.stderr.on('data', data => console.log(`Error stream received data: ${data}`));
-
-worker.on('error', () => console.log('Some error occured'));
